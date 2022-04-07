aacf42c2f4b72e3a1948db5bbb689495b0bcf1e0
nabe-kurage <tabiflag@gmail.com>
2022-04-07 21:46:03 +0900

    add other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 // Switchは今なくて、Routes を使っておなじことができる
-import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile } from "./pages/Profile";
 import { Work } from "./pages/Work";
 
@@ -8,7 +8,6 @@ function App() {
     return (
         <div className="root">
             <h1>my site</h1>
-            <p>test</p>
             <ul className="linkList">
                 <li className="linkListItem">
                     <a className="link" href="/self-introduction-site/profile">
@@ -32,7 +31,6 @@ function App() {
                 <Routes>
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/works" element={<Work />} />
-                    <Redirect to="/" />
                 </Routes>
             </BrowserRouter>
         </div>
